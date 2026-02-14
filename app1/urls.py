070b0272850070fef1748a87e6e8d8dc652d9c7e
Harishyokesh <harishyokesh551561@gmail.com>
2026-02-14 07:09:29 +0530

    updated project

--- app1/urls.py
+++ app1/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('students/',views.show_students,name="students"),
-    path('about/',views.about,name="about")
+    path('about/',views.about,name="about"),
+    path('address/',views.about,name="about")
 ]
